0e96d990465bb0a37070f1271e51443836b92b22
NicoCPH <nicolas2990@hotmail.com>
2023-07-19 19:28:19 +0200

    merged

--- app/routes/privacy-policy.tsx
+++ app/routes/privacy-policy.tsx
@@ -1,4 +1,4 @@
-import type { V2_MetaFunction, LinksFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from "@remix-run/node";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Privacy from "../components/Privacy"
