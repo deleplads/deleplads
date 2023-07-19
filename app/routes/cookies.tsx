244591bc224559f6a8bef46436102bc9de830691
NicoCPH <nicolas2990@hotmail.com>
2023-07-19 19:35:37 +0200

    merged

--- app/routes/cookies.tsx
+++ app/routes/cookies.tsx
@@ -1,7 +1,7 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Cookies from "../components/Cookies"
+import Cookies from "../components/cookies"
 
 export const meta: V2_MetaFunction = () => {
   return [
