2cd73870b9af2bd78090a8a57fad82683f78d0c3
unknown <bentreadwell>
2023-09-07 19:19:10 +0200

    Har tilføjet skelet til kortvisning.

---
+++ app/components/map.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function Map() {
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+
+  function handleApiLoaded(map: any, maps: any): void {
+    throw new Error("Function not implemented.");
+  }
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBtz-re5-odkjXIJnFqMiWJVDo8z6jpP9g" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+      >
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+      </GoogleMapReact>
+    </div>
+  );
+}
