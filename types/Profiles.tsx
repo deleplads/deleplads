aa93e3615e0cc635e3fb6a7c0a43deb526e8fd2d
Cristian Pandele <cristianpand@yahoo.com>
2023-11-24 19:58:13 +0100

    Revert "Revert "Refactor""
    This reverts commit 23f737165549c6c97c29f1c8413d8be4fd793f10.

---
+++ types/Profiles.tsx
@@ -0,0 +1,11 @@
+export interface updateProfiles {
+    firstName: string;
+    lastName: string;
+    address?: string;
+    postalCode?: string;
+    city?: string;
+    birthYear?: string;
+    birthMonth?: string;
+    birthDay?: string;
+    phoneNumber?: string;
+}
