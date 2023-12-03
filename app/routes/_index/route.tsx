c68209d5644287c17a05111466d4f7055e12443e
Cristian Pandele <cristianpand@yahoo.com>
2023-12-03 10:39:03 +0100

    Login cleanup of test code

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,4 +1,4 @@
-import type { LoaderFunction, V2_MetaFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from "@remix-run/node";
 import CallToAction from "~/components/CallToAction";
 import Footer from "~/components/Footer";
 import Hero from "~/components/Hero";
@@ -7,26 +7,9 @@ import SocialProof from "~/components/blogTeaser";
 import FAQGeneral from "~/components/FAQGeneral";
 import swipercss from "node_modules/swiper/swiper-bundle.min.css";
 import Benefits from "~/components/benefits";
-import { getAllStuff } from "utils/stuff.server";
-import { useLoaderData } from "@remix-run/react";
 
-
-export const loader: LoaderFunction = async ({ request }) => {
-
-  // const stuff = await getAllStuff();
-  // console.log(stuff)
-
-  // console.log(stuff.map(item => item.name))
-
-  return { name: "hello" };
-
-}
 
 export const meta: V2_MetaFunction = () => {
-  const data = useLoaderData();
-  console.log(data)
-  // const stuff = await getAllStuff();
-
   return [
     { title: "Deleplads.dk" },
     { name: "description", content: "Welcome to Remix!" },
--- utils/prisma.server.ts
+++ utils/prisma.server.ts
@@ -1,30 +1,6 @@
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient();
 
-// const prisma = new PrismaClient({
-//   datasources: { db: { url: process.env.DATABASE_URL } },
-// }).$extends(useSupabaseRowLevelSecurity())
-
-const prisma = new PrismaClient()
-const rlsDb = prisma.$extends(currentCompanyRlsExtension(session.stuff.userId))
-
-function currentCompanyRlsExtension(userId) {
-  return Prisma.defineExtension((prisma) =>
-    prisma.$extends({
-      query: {
-        $allModels: {
-          async $allOperations({ args, query }) {
-            const [, result] = await prisma.$transaction([
-              prisma.$executeRaw`SELECT set_config('user_id', ${userId}, TRUE)`,
-              query(args),
-            ])
-            return result
-          },
-        },
-      },
-    }),
-  )
-}
+const prisma = new PrismaClient();
 
 export { prisma }
