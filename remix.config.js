f406fab189b5c56d098fab32f33e7b489ae02b4d
Nicolas Allesøe <58513123+NicoCPH@users.noreply.github.com>
2023-12-04 20:16:14 +0100

    test

--- remix.config.js
+++ remix.config.js
@@ -4,8 +4,23 @@ const { flatRoutes } = require('remix-flat-routes')
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   routes: async defineRoutes => {
-    return flatRoutes('routes', defineRoutes)
-  },
+    return defineRoutes((route) => {
+        route("/", "routes/_index/route.tsx", { index: true });
+        route("rental", "root.tsx", () => {
+          route("/", "routes/rental/_index.tsx", { index: true }); // Assuming there's a default rental page
+          route(":id/attributes", "routes/rental/$id_.attributes.tsx");
+          route(":id/availability-single", "routes/rental/$id_.avaliability_.single.tsx");
+          route(":id/availability-type", "routes/rental/$id_.avaliability_.type.tsx");
+          route(":id/availability", "routes/rental/$id_.avaliability.tsx");
+          route(":id/images", "routes/rental/$id_.images.tsx");
+          route(":id/location", "routes/rental/$id_.location.tsx");
+          route(":id/notes", "routes/rental/$id_.notes.tsx");
+          route(":id/price", "routes/rental/$id_.price.tsx");
+          route(":id/receipt", "routes/rental/$id_.receipt.tsx");
+          route(":id/type", "routes/rental/$id_.type.tsx");
+        });
+      });
+    },
   // When running locally in development mode, we use the built-in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
