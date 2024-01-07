a7a086a88a7cb9fcdb0a5c9a8b0a054037c44cb6
Nicolas Allesøe <58513123+NicoCPH@users.noreply.github.com>
2024-01-07 22:00:28 +0100

    Update Cards.tsx

--- app/components/Parkingspots/Cards.tsx
+++ app/components/Parkingspots/Cards.tsx
@@ -11,7 +11,7 @@ export default function BasicCard(spot: ParkingSpot) {
   return (
     <div
       onClick={() => {
-        navigate(`parkeringsplads/${parkingspot.id}`);
+        navigate(`/parkeringsplads/${parkingspot.id}`);
       }}
     >
       <Card className="gallery-cards">
